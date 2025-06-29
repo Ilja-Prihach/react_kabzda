fb39989de7e82978738fd05c22660fdc4555e01b
Ilja-Prihach <piratehnik9508@gmail.com>
2025-06-29 13:08:57 +0300

    sprint 1, week 1,2,3

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import  Rating from "./components/Rating/Rating";
+
+
+function App() {
+  return (
+      <div>
+          <PageTitle title={"This is App component"}/>
+          <PageTitle title={"My Friends"}/>
+          Article 1
+          <Rating value={3}/>
+          <Accordion titleValue={"Menu"} collapsed={true}/>
+          <Accordion titleValue={"Users"} collapsed={false}/>
+          Article 2
+          <Rating value={4}/>
+      </div>
+
+  )
+}
+
+type PageTitlePropsType = {
+    title: string;
+}
+
+function PageTitle(props: PageTitlePropsType) {
+  console.log("AppTitle rendering");
+  return <h1>{ props.title }</h1>
+}
+
+
+
+
+
+export default App;
