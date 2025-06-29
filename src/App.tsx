0eb81dc02963c82744d5d7937a27dd12b70a4544
Ilja-Prihach <piratehnik9508@gmail.com>
2025-06-29 14:45:02 +0300

    create components OnOff

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,21 @@ import React from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import  Rating from "./components/Rating/Rating";
+import OnOff from "./components/OnOff/OnOff";
 
 
 function App() {
   return (
       <div>
           <PageTitle title={"This is App component"}/>
-          <PageTitle title={"My Friends"}/>
+          {/*<PageTitle title={"My Friends"}/>*/}
           Article 1
           <Rating value={3}/>
           <Accordion titleValue={"Menu"} collapsed={true}/>
           <Accordion titleValue={"Users"} collapsed={false}/>
-          Article 2
-          <Rating value={4}/>
+          {/*Article 2*/}
+          {/*<Rating value={4}/>*/}
+          <OnOff on={true}/>
       </div>
 
   )
---
+++ src/components/OnOff/OnOff.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+type OnOffPropsType = {
+    on: boolean
+}
+
+function OnOff(props: OnOffPropsType){
+    const  onStyle = {
+        width: "35px",
+        height: "15px",
+        border: "1px solid black",
+        display: "inline-block",
+        padding: "2px",
+        backgroundColor: props.on ? "#9df506" : "#FFFFFF"
+    };
+    const  offStyle = {
+        width: "35px",
+        height: "15px",
+        border: "1px solid black",
+        display: "inline-block",
+        padding: "2px",
+        marginLeft: "2px",
+        backgroundColor: props.on ? "#FFFFFF" : "#e81212"
+    };
+    const indicatorStyle = {
+        width: "15px",
+        height: "15px",
+        borderRadius: "50%",
+        border: "1px solid black",
+        display: "inline-block",
+        marginLeft: "5px",
+        backgroundColor: props.on ? "#9df506" : "#e81212"
+    }
+    return (
+        <div>
+            <div style={onStyle}>On</div>
+            <div style={offStyle}>Off</div>
+            <div style={indicatorStyle}></div>
+        </div>
+    )
+}
+
+export default OnOff;
